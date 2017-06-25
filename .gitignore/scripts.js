16a150386eebdb79de6390e51c046455bbae59c8
tlmca <lovey.torey098@gmail.com>
2017-06-24 17:08:05 -0700

    Add files via upload

---
+++ .gitignore/scripts.js
@@ -0,0 +1,11 @@
+/* JAVASCRIPT. Uncomment this when you want to play with Javascript!
+
+function dateFunction() {
+    document.getElementById("demo").innerHTML = Date();
+}
+
+function helloWorld() {
+    alert("I just wrote Javascript!");
+}
+
+////////////////////////////////////////////////////////////////// */
